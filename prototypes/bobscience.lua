dc6ecb3df6c544403bc4769fdd84e472420112dd
Shane Madden <shanemadden@fake.email>
2019-03-31 14:45:11 -0600

    Fix checking existence of Bob's packs

--- prototypes/bobscience.lua
+++ prototypes/bobscience.lua
@@ -7,12 +7,31 @@ deadlock.add_stack("production-science-pack", nil, "deadlock-stacking-3", 32, "t
 deadlock.add_stack("utility-science-pack", nil, "deadlock-stacking-3", 32, "tool")
 deadlock.add_stack("space-science-pack", nil, "deadlock-stacking-3", 32, "tool")
 -- bob's
-deadlock.add_stack("advanced-logistic-science-pack", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("science-pack-gold", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-blue", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-orange", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-purple", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-yellow", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-green", nil, "deadlock-stacking-2", 32, "tool")
-deadlock.add_stack("alien-science-pack-red", nil, "deadlock-stacking-2", 32, "tool")
+
+if data.raw.tool["advanced-logistic-science-pack"] then
+	deadlock.add_stack("advanced-logistic-science-pack", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["science-pack-gold"] then
+	deadlock.add_stack("science-pack-gold", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack"] then
+	deadlock.add_stack("alien-science-pack", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-blue"] then
+	deadlock.add_stack("alien-science-pack-blue", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-orange"] then
+	deadlock.add_stack("alien-science-pack-orange", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-purple"] then
+	deadlock.add_stack("alien-science-pack-purple", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-yellow"] then
+	deadlock.add_stack("alien-science-pack-yellow", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-green"] then
+	deadlock.add_stack("alien-science-pack-green", nil, "deadlock-stacking-2", 32, "tool")
+end
+if data.raw.tool["alien-science-pack-red"] then
+	deadlock.add_stack("alien-science-pack-red", nil, "deadlock-stacking-2", 32, "tool")
+end
