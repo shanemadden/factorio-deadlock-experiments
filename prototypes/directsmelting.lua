4df4fa340b1b980e2a5988d22803383fc63c34da
Shane Madden <shanemadden@fake.email>
2019-04-06 11:19:56 -0600

    Scale direct smelting recipes appropriately when the stack size has been adjusted on the base mod

--- prototypes/directsmelting.lua
+++ prototypes/directsmelting.lua
@@ -1,45 +1,49 @@
+local copper_density = deadlock.get_item_stack_density("copper-ore", "item")
 local copper = {
   type = "recipe",
   name = "stacked-copper-plate",
   category = "smelting",
-  energy_required = 16,
+  energy_required = 3.2 * copper_density,
   ingredients = {{ "deadlock-stack-copper-ore", 1}},
 }
 if settings.startup["experiments-directsmelting-outputstacks"].value then
   copper.result = "deadlock-stack-copper-plate"
 else
   copper.result = "copper-plate"
-  copper.result_count = 5
+  copper.result_count = copper_density
 end
 
+local iron_density = deadlock.get_item_stack_density("iron-ore", "item")
 local iron = {
   type = "recipe",
   name = "stacked-iron-plate",
   category = "smelting",
-  energy_required = 16,
+  energy_required = 3.2 * iron_density,
   ingredients = {{ "deadlock-stack-iron-ore", 1}},
 }
 if settings.startup["experiments-directsmelting-outputstacks"].value then
   iron.result = "deadlock-stack-iron-plate"
 else
   iron.result = "iron-plate"
-  iron.result_count = 5
+  iron.result_count = iron_density
 end
 
+local stone_density = deadlock.get_item_stack_density("stone", "item")
 local stone = {
   type = "recipe",
   name = "stacked-stone-brick",
   category = "smelting",
-  energy_required = 16,
+  energy_required = 3.2 * stone_density,
   ingredients = {{ "deadlock-stack-stone", 1}},
 }
 if settings.startup["experiments-directsmelting-outputstacks"].value then
   stone.result = "deadlock-stack-stone-brick"
 else
   stone.result = "stone-brick"
-  stone.result_count = 5
+  stone.result_count = stone_density
 end
 
+local iron_plate_density = deadlock.get_item_stack_density("iron-plate", "item")
 local steel = {
   type = "recipe",
   name = "stacked-steel-plate",
@@ -48,30 +52,30 @@ local steel = {
 if settings.startup["experiments-directsmelting-outputstacks"].value then
   steel.normal = {
     enabled = false,
-    energy_required = 80,
+    energy_required = 16 * iron_plate_density,
     ingredients = {{"deadlock-stack-iron-plate", 5}},
     result = "deadlock-stack-steel-plate"
   }
   steel.expensive = {
     enabled = false,
-    energy_required = 160,
+    energy_required = 32 * iron_plate_density,
     ingredients = {{"deadlock-stack-iron-plate", 10}},
     result = "deadlock-stack-steel-plate"
   }
 else
   steel.normal = {
     enabled = false,
-    energy_required = 80,
+    energy_required = 16 * iron_plate_density,
     ingredients = {{"deadlock-stack-iron-plate", 5}},
     result = "steel-plate",
-    result_count = 5,
+    result_count = iron_plate_density,
   }
   steel.expensive = {
     enabled = false,
-    energy_required = 160,
+    energy_required = 32 * iron_plate_density,
     ingredients = {{"deadlock-stack-iron-plate", 10}},
     result = "steel-plate",
-    result_count = 5,
+    result_count = iron_plate_density,
   }
 end
 data:extend({copper, iron, stone, steel})
