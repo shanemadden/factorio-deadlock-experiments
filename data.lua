a32afa5d42434a3aca0ae1fbbcec9c5e0c95e2d8
Simon Bürger <simon.buerger@rwth-aachen.de>
2020-12-13 22:39:36 +0100

    Direct crafting (#2)
    * add direct crafting of stacked gears, circuits, plastic
    
    * unlock direct crafting recipes by appropriate techs

--- data.lua
+++ data.lua
@@ -4,3 +4,6 @@ end
 if settings.startup["experiments-directsmelting"].value then
 	require("prototypes.directsmelting")
 end
+if settings.startup["experiments-directcrafting"].value then
+	require("prototypes.directcrafting")
+end
